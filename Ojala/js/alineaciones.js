5d5a879f17d662aee6e298589af92a78f33d62a5
David Salinas <dnarino@gmail.com>
2017-07-12 12:47:39 -0500

    añadiendo animacion de dezplazamiento

--- Ojala/js/alineaciones.js
+++ Ojala/js/alineaciones.js
@@ -12,24 +12,48 @@ var miAside = document.querySelector('.miAside');
 
 //capturando evento botones:click
 
+//boton izquierdo
+
 botonLeft.addEventListener('click', function (event) {
+    miAside.classList.remove('flex-last','invisible','transitions2','transitions1','col-md-12');
 
     if (!miAside.classList.contains('flex-first')) {
-        // console.log(miAside.classList.contains('flex-first'));
         miAside.classList.add('flex-first');
-    } 
-
+    }
+    miAside.classList.add('transitions1');
+    console.log(miAside);
 });
 
+//boton centro
 botonCenter.addEventListener('click', function (event) {
-    alert("Le diste click al boton centro");
+    miAside.classList.remove('flex-last', 'invisible','transitions','transitions1');
+    if (!miAside.classList.contains('col-md-12')) {
+        miAside.classList.add('col-md-12','flex-first');
+    }else{
+        miAside.classList.remove('col-md-12', 'flex-first');
+    }
+    miAside.classList.add('transitions2');
+    console.log(miAside);
 });
+
+//boton derecho
 
 botonRight.addEventListener('click', function (event) {
-    
-    alert("Le diste click al boton derecho");
+    miAside.classList.remove('flex-first', 'invisible', 'col-md-12','transitions2','transitions1');
+    if (!miAside.classList.contains('flex-last')) {
+        miAside.classList.add('flex-last');
+    }
+    miAside.classList.add('transitions');
 });
 
+//boton hide
 botonHide.addEventListener('click', function (event) {
-    alert("Le diste click al boton de esconder");
+
+    miAside.classList.remove('flex-first', 'flex-last','transitions','transitions1');
+    if (miAside.classList.contains('invisible')) {
+        miAside.classList.remove('invisible');
+    } else {
+        miAside.classList.add('invisible');
+    }
+    console.log(miAside);
 });
